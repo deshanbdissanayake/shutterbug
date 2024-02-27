5a484072f3e6cbcce9c524929dfd53448a9acd23
Deshan Dissanayake <113162275+deshanbdissanayake@users.noreply.github.com>
2024-02-27 14:51:57 +0530

    20240227-1451-desh

--- assets/data/news.js
+++ assets/data/news.js
@@ -1,34 +1,40 @@
-const getNews = [
-    {
+const getNews = async () => {
+    let data = [
+        {
+            id: 1,
+            title: "30%",
+            sub_title: "Today's special",
+            news: "Get discount for every order, only valid for today.",
+            image: "https://shutterbug.introps.com/documents/news/test-1.jpg"
+        },
+        {
+            id: 2,
+            title: "10%",
+            sub_title: "Day after Tomorrow's special",
+            news: "Get discount for every order, only valid for Day after Tomorrow.",
+            image: "https://shutterbug.introps.com/documents/news/test-3.jpg"
+        },
+        {
+            id: 3,
+            title: "10%",
+            sub_title: "Valentine Special",
+            news: "Special offer for lovebirds.",
+            image: "https://shutterbug.introps.com/documents/news/test-4.jpg"
+        },
+    ];
+    return data;
+}
+
+const getNewsById = async () => {
+    let data = {
         id: 1,
         title: "30%",
         sub_title: "Today's special",
         news: "Get discount for every order, only valid for today.",
-        image: "https://shutterbug.introps.com/documents/news/test-1.jpg"
-    },
-    {
-        id: 2,
-        title: "10%",
-        sub_title: "Day after Tomorrow's special",
-        news: "Get discount for every order, only valid for Day after Tomorrow.",
-        image: "https://shutterbug.introps.com/documents/news/test-3.jpg"
-    },
-    {
-        id: 3,
-        title: "10%",
-        sub_title: "Valentine Special",
-        news: "Special offer for lovebirds.",
-        image: "https://shutterbug.introps.com/documents/news/test-4.jpg"
-    },
-];
-
-const getNewsById = {
-    id: 1,
-    title: "30%",
-    sub_title: "Today's special",
-    news: "Get discount for every order, only valid for today.",
-    image: "https://shutterbug.introps.com/documents/news/test-1.jpg",
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget feugiat metus. Aenean faucibus vulputate sapien, et pretium risus pharetra sed. Nullam varius ac enim eu pharetra. Donec eget mollis purus. Suspendisse potenti. Sed luctus nibh cursus fringilla laoreet. Aenean scelerisque, orci vestibulum aliquam egestas, dui nulla aliquet eros, posuere mattis quam velit et massa. Nulla tristique, massa bibendum egestas consequat, dui purus ornare ante, vel venenatis quam nunc sit amet libero. Fusce vel dui vitae eros luctus aliquet ac vel ex. Nullam luctus luctus dui quis mollis.',
+        image: "https://shutterbug.introps.com/documents/news/test-1.jpg",
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget feugiat metus. Aenean faucibus vulputate sapien, et pretium risus pharetra sed. Nullam varius ac enim eu pharetra. Donec eget mollis purus. Suspendisse potenti. Sed luctus nibh cursus fringilla laoreet. Aenean scelerisque, orci vestibulum aliquam egestas, dui nulla aliquet eros, posuere mattis quam velit et massa. Nulla tristique, massa bibendum egestas consequat, dui purus ornare ante, vel venenatis quam nunc sit amet libero. Fusce vel dui vitae eros luctus aliquet ac vel ex. Nullam luctus luctus dui quis mollis.',
+    }
+    return data;
 }
 
 export { getNews, getNewsById }
--- screens/client/HomeScreen/NewsSec.js
+++ screens/client/HomeScreen/NewsSec.js
@@ -1,12 +1,26 @@
 import { StyleSheet, View, Dimensions } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Carousel from 'react-native-reanimated-carousel';
 
 import { getNews } from '../../../assets/data/news';
 import NewsSliderSingle from '../../../components/app/NewsSliderSingle'
 
 const NewsSec = () => {
+  const [news, setNews] = useState(null);
   const width = Dimensions.get('window').width;
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        let data = await getNews();
+        setNews(data);
+      } catch (error) {
+        console.error('error at fetching news', error)
+        setNews(null);
+      }
+    }
+    getData()
+  },[])
 
   const handleViewMoreClick = (clickedNewsId) => {
     console.log(clickedNewsId)
@@ -14,16 +28,18 @@ const NewsSec = () => {
 
   return (
     <View style={styles.container}>
-      <Carousel
-        loop
-        width={width}
-        autoPlay={true}
-        data={getNews}
-        scrollAnimationDuration={4000}
-        renderItem={({ item }) => (
-          <NewsSliderSingle newsItem={item} handleViewMoreClick={handleViewMoreClick} />
-        )}
-      />
+      {(news && news.length > 0) && (
+        <Carousel
+          loop
+          width={width}
+          autoPlay={true}
+          data={news}
+          scrollAnimationDuration={4000}
+          renderItem={({ item }) => (
+            <NewsSliderSingle newsItem={item} handleViewMoreClick={handleViewMoreClick} />
+          )}
+        />
+      )}
     </View>
   )
 }
